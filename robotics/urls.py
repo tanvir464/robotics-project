fb839b5968c3fdc22ec5ce836108148e1d9ba8f6
Tanvir Ahmed <100764069+tanvir464@users.noreply.github.com>
2025-02-08 15:27:54 +0600

    feat: update URL routing to use distill_url for home, about, and weekly views

--- robotics/urls.py
+++ robotics/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
 from django.contrib import admin
-from django_distill import distill_path
-from home import views
+from django_distill import distill_url
+
+from home import views as home_views
+from about import views as about_views
+from weekly import views as weekly_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    distill_url('', home_views.home, name='home'),
+    distill_url('about/', about_views.about, name='about'),
+    distill_url('contact/', weekly_views.weekly, name='weekly'),
     path('', include('base.urls')),
-    distill_path('', views.home, name='home'),
 ]
