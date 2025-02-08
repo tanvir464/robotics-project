d2f55990043e4a2ca29eb0fa9afa22bc98aadce7
Tanvir Ahmed <100764069+tanvir464@users.noreply.github.com>
2025-02-08 14:51:17 +0600

    refactor: remove unused static files and server function, update URLs for home view

--- netlify/functions/server.py
+++
@@ -1,4 +0,0 @@
-from robotics.wsgi import application
-
-def handler(event, context):
-    return application(event, context)
--- robotics/urls.py
+++ robotics/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
+from django_distill import distill_path
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    distill_path('', views.home, name='home'),
 ]
